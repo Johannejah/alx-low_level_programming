5ec148ed2d43a7a1dddd3ccd1204807e2f2ddbec
Johannejah <Johannejah@gmail.com>
2023-03-20 19:26:46 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,14 +12,13 @@ int _strlen(char *s)
 	int i;
 
 	i = 0;
-
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-
 	return (i);
 }
+
 
 /**
  * *_strcpy - copies the string pointed to by src
@@ -35,20 +34,18 @@ char *_strcpy(char *dest, char *src)
 	int len, i;
 
 	len = 0;
-
 	while (src[len] != '\0')
 	{
 		len++;
 	}
-
 	for (i = 0; i < len; i++)
 	{
 		dest[i] = src[i];
 	}
 	dest[i] = '\0';
-
 	return (dest);
 }
+
 
 /**
  * new_dog - creates a new dog
@@ -58,6 +55,7 @@ char *_strcpy(char *dest, char *src)
  *
  * Return: pointer to the new dog (Success), NULL otherwise
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
@@ -65,12 +63,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
-
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-
 	dog->name = malloc(sizeof(char) * (len1 + 1));
+
 	if (dog->name == NULL)
 	{
 		free(dog);
@@ -78,7 +75,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	dog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (dog->owner == NULL)
-
+	{
 		free(dog);
 		free(dog->name);
 		return (NULL);
@@ -86,6 +83,5 @@ dog_t *new_dog(char *name, float age, char *owner)
 	_strcpy(dog->name, name);
 	_strcpy(dog->owner, owner);
 	dog->age = age;
-
 	return (dog);
 }
